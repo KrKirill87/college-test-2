f63a464cf9f51a4673437e307840caa939358cdf
KrKirill87 <fanofzenitkirill@gmail.com>
2023-05-30 22:28:31 +0300

    some fix

--- index.js
+++ index.js
@@ -24,12 +24,13 @@ const content = fs.readFileSync(path.join(
 
 
 // BEGIN
-//console.log(content)
+console.log(content)
 let data = content
-  .split("\r\n")
+  .replaceAll(' ', '')
+  .split('\r\n')
   .slice(1)
-  .map((init) => init.slice(2, -2).split (' | '));//[0]-unit,[1]-power,[2]-health,[3]-qountity,[4]-height,[5]-weight,[6]-price
-  
+  .map((init) => init.slice(1, -1).split ('|'));//[0]-unit,[1]-power,[2]-health,[3]-qountity,[4]-height,[5]-weight,[6]-price
+console.log(data)
 //1 задание
 console.log(`количество существ: ${data.length}`);
 
